6ee2249637e1717009984d12f2c844d50eb577ac
CharmanderHero190 <adko214@azet.sk>
2025-09-07 19:07:35 +0200

    Update doomWADBuilder.js

--- doomWADBuilder.js
+++ doomWADBuilder.js
@@ -1,77 +1,55 @@
 (function() {
-    class DoomWADBuilder {
-        constructor(runtime) {
-            this.runtime = runtime;
-            this.wadName = "newwad";
-            this.rooms = [];
-        }
+    const ext = {};
+    let wadName = "newwad";
+    let rooms = [];
 
-        // Set WAD Name
-        setWADName(name) {
-            this.wadName = name || "newwad";
-        }
+    // Required cleanup function
+    ext._shutdown = function() {};
 
-        // Make Cube Room
-        makeCubeRoom(x, y, size) {
-            const room = { id: this.rooms.length + 1, x, y, width: size, height: size, depth: size };
-            this.rooms.push(room);
-            return room.id;
-        }
+    // Status reporting
+    ext._getStatus = function() {
+        return { status: 2, msg: 'Ready' };
+    };
 
-        // Download WAD using data URL (works in TurboWarp)
-        downloadWAD(filename) {
-            const wadData = JSON.stringify({ name: this.wadName, rooms: this.rooms }, null, 2);
-            const dataUrl = 'data:text/json;charset=utf-8,' + encodeURIComponent(wadData);
+    // Set WAD Name
+    ext.setWADName = function(name) {
+        wadName = name || "newwad";
+    };
 
-            const link = document.createElement('a');
-            link.href = dataUrl;
-            link.download = filename || this.wadName + ".wad.json";
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        }
+    // Make Cube Room
+    ext.makeCubeRoom = function(x, y, size) {
+        const room = { id: rooms.length + 1, x, y, width: size, height: size, depth: size };
+        rooms.push(room);
+        return room.id;
+    };
 
-        // Block definitions
-        getInfo() {
-            return {
-                id: 'doomWADBuilder',
-                name: 'Doom WAD Builder',
-                blocks: [
-                    {
-                        opcode: 'setWADName',
-                        blockType: 'command',
-                        text: 'Set WAD Name [NAME]',
-                        arguments: {
-                            NAME: { type: 'string', defaultValue: 'MyWAD' }
-                        }
-                    },
-                    {
-                        opcode: 'makeCubeRoom',
-                        blockType: 'reporter',
-                        text: 'Make Cube Room at X [X] Y [Y] Size [SIZE]',
-                        arguments: {
-                            X: { type: 'number', defaultValue: 0 },
-                            Y: { type: 'number', defaultValue: 0 },
-                            SIZE: { type: 'number', defaultValue: 64 }
-                        }
-                    },
-                    {
-                        opcode: 'downloadWAD',
-                        blockType: 'command',
-                        text: 'Download WAD as [FILENAME]',
-                        arguments: {
-                            FILENAME: { type: 'string', defaultValue: 'MyWAD.wad.json' }
-                        }
-                    }
-                ]
-            };
-        }
-    }
+    // Download WAD (works in TurboWarp)
+    ext.downloadWAD = function(filename) {
+        const wadData = JSON.stringify({ name: wadName, rooms: rooms }, null, 2);
+        const dataUrl = 'data:text/json;charset=utf-8,' + encodeURIComponent(wadData);
 
-    // Register the extension with TurboWarp VM
-    if (typeof window.vm !== 'undefined') {
-        window.vm.extensionManager.register(new DoomWADBuilder(window.vm));
+        const link = document.createElement('a');
+        link.href = dataUrl;
+        link.download = filename || wadName + ".wad.json";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
+    // Block definitions
+    const descriptor = {
+        blocks: [
+            [' ', 'Set WAD Name %s', 'setWADName', 'MyWAD'],
+            ['r', 'Make Cube Room at X %n Y %n Size %n', 'makeCubeRoom', 0, 0, 64],
+            [' ', 'Download WAD as %s', 'downloadWAD', 'MyWAD.wad.json']
+        ],
+        url: 'https://example.com' // optional
+    };
+
+    // Register extension
+    if (typeof ScratchExtensions !== "undefined") {
+        ScratchExtensions.register('Doom WAD Builder', descriptor, ext);
     } else {
-        console.warn('TurboWarp VM not detected. Load this extension via URL inside TurboWarp.');
+        console.warn('Load this JS via TurboWarp Extensions → Add Extension → URL');
     }
 })();
